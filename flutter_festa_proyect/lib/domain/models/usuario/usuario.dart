b90e476ff5915faa64bf686c412b68f55465ea1c
juan <juan.1701622573@gmail.com>
2022-11-14 13:57:35 -0500

    estructura backend

---
+++ flutter_festa_proyect/lib/domain/models/usuario/usuario.dart
@@ -0,0 +1,17 @@
+class Usuario {
+  int documentoIdentidad;
+  String nombre;
+  int edad;
+  int numeroCelular;
+  String correoElectronico;
+  String contrasena;
+
+  Usuario(
+    this.documentoIdentidad,
+    this.nombre,
+    this.edad,
+    this.numeroCelular,
+    this.correoElectronico,
+    this.contrasena,
+  );
+}
