2d558af0e1dba456b418e3e3ade30188fd6e4df1
Jose Manuel Marin <jose.1701722372@ucaldas.edu.co>
2022-11-16 11:35:25 -0500

    Proyect Clean

--- flutter_festa_proyect/lib/config/provider/event_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_festa_proyect/domain/use_case/event/cu_list_events.dart';
-
-class EventProvider extends ChangeNotifier{
-
-  final CuListEvents cuListEvents;
-  EventProvider({required this.cuListEvents});
-}
