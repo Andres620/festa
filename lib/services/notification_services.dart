fa2a4c0beaca878b73eda4accde720b3f89a3cce
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-16 14:32:27 -0500

    Serivcio de notificaciones

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -11,7 +11,7 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 Future<void> initNotifications() async {
   // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('festa-alien');
+      AndroidInitializationSettings('festa_alien');
 
   final DarwinInitializationSettings initializationSettingsIOS =
       DarwinInitializationSettings();
