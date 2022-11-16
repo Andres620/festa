61390199a163da0e3c9c8c4bb219468650f2c5c9
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-16 13:36:16 -0500

    [add] services - notifications_services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:festa/app.dart';
+import 'package:festa/services/notification_services.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initNotifications();
   runApp(const Festa());
 }
