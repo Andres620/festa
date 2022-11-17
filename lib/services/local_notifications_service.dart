6a04551912a28607e98a3fca3619ac46ecebae94
Juan Felipe Ospina <61992581+PildoDev@users.noreply.github.com>
2022-11-17 17:07:47 -0500

    Juan ospina branch - Backend listar eventos por discoteca (#24)
    * Backend listar eventos por discoteca
    
    * Create repository and user fake db generator, and create level class
    
    Co-authored-by: juan <juan.1701622573@gmail.com>

--- lib/services/local_notifications_service.dart
+++ lib/services/local_notifications_service.dart
@@ -8,7 +8,6 @@ import 'package:timezone/data/latest.dart' as tz;
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-
 Future<void> initialize() async {
   tz.initializeTimeZones();
   // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
@@ -49,74 +48,65 @@ Future<NotificationDetails> _notificationDetails() async {
 }
 
 Future<void> showNotification(
-  // {
-  // required int id,
-  // required String title,
-  // required String body,}
-  ) async {
+    // {
+    // required int id,
+    // required String title,
+    // required String body,}
+    ) async {
   final notificationDetails = await _notificationDetails();
   await flutterLocalNotificationsPlugin.show(
-      1,
-      'Titulo de notificacion',
-      'hola de saludo.',
-      notificationDetails);
+      1, 'Titulo de notificacion', 'hola de saludo.', notificationDetails);
 }
 
-  Future<void> showScheduledNotification(
-      // {required int id,
-      // required String title,
-      // required String body,
-      // required int seconds}
-      ) async {
-    final notificationDetails = await _notificationDetails();
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      1,
-      'Titulo de notificacion',
-      'hola de saludo.',
-      tz.TZDateTime.from(
-        DateTime.now().add(Duration(seconds: 4)),
-        tz.local,
-      ),
-      notificationDetails,
-      androidAllowWhileIdle: true,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-    );
-  }
+Future<void> showScheduledNotification(
+    // {required int id,
+    // required String title,
+    // required String body,
+    // required int seconds}
+    ) async {
+  final notificationDetails = await _notificationDetails();
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    1,
+    'Titulo de notificacion',
+    'hola de saludo.',
+    tz.TZDateTime.from(
+      DateTime.now().add(Duration(seconds: 4)),
+      tz.local,
+    ),
+    notificationDetails,
+    androidAllowWhileIdle: true,
+    uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+  );
+}
 
-    Future<void> showIntervbalNotification(
-      // {required int id,
-      // required String title,
-      // required String body,
-      // required int seconds}
-      ) async {
-    final notificationDetails = await _notificationDetails();
-    await flutterLocalNotificationsPlugin.periodicallyShow(
+Future<void> showIntervbalNotification(
+    // {required int id,
+    // required String title,
+    // required String body,
+    // required int seconds}
+    ) async {
+  final notificationDetails = await _notificationDetails();
+  await flutterLocalNotificationsPlugin.periodicallyShow(
       1,
       'Titulo de notificacion',
       'hola de saludo.',
       RepeatInterval.everyMinute,
       notificationDetails,
-      androidAllowWhileIdle: true
-    );
-  }
+      androidAllowWhileIdle: true);
+}
 
-
-  Future<void> showNotificationWithPayload(
-      // {required int id,
-      // required String title,
-      // required String body,
-      // required String payload}
-      {required String payload}
-      ) async {
-    final notificationDetails = await _notificationDetails();
-    await flutterLocalNotificationsPlugin.show(
-       1,
-      'Titulo de notificacion',
-      'hola de saludo.',
-      notificationDetails,
+Future<void> showNotificationWithPayload(
+    // {required int id,
+    // required String title,
+    // required String body,
+    // required String payload}
+    {required String payload}) async {
+  final notificationDetails = await _notificationDetails();
+  await flutterLocalNotificationsPlugin.show(
+      1, 'Titulo de notificacion', 'hola de saludo.', notificationDetails,
       payload: payload);
-  }
+}
 
 void onDidReceiveLocalNotification(
     int id, String? title, String? body, String? payload) {
