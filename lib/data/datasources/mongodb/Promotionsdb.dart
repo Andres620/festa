346399584298695c263e0b1771a35d6599f1b54e
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-18 00:36:52 -0500

    add Connection MongoDB

---
+++ lib/data/datasources/mongodb/Promotionsdb.dart
@@ -0,0 +1,12 @@
+// ignore: file_names
+import '../../../domain/models/promocion.dart';
+import '../../../domain/repositories/promocion_repository.dart';
+
+class Promotionsdb extends PromocionRepository{
+
+  @override
+  Future<List<Promocion>> getPromociones() {
+    // TODO: implement getPromociones
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:festa/app.dart';
 import 'package:festa/services/local_notifications_service.dart';
-// import 'package:festa/services/notification_services.dart';
 import 'package:flutter/material.dart';
+import 'data/datasources/mongodb/mongodb.dart';
 
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initialize();
+  await MongoDatabase.connect();
   runApp(const Festa());
 }
