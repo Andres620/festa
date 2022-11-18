381fd5a58237ae5cea0a7a69015705abf6cdec52
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-17 23:18:32 -0500

    working on MongoDB connection

--- lib/config/provider/event_provider.dart
+++ lib/config/provider/event_provider.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../domain/use_case/cu_list_events.dart';
 
 class EventProvider extends ChangeNotifier{
-
   final CuListEvents cuListEvents;
   EventProvider({required this.cuListEvents});
 }
