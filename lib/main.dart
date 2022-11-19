a67efa9833a11ae89fc2cd17eb7b3c2bc4ad1e3d
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-19 18:26:29 -0500

    [Add] getARandomEvent on events_mongodb

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:festa/app.dart';
 import 'package:festa/services/local_notifications_service.dart';
 import 'package:flutter/material.dart';
 
+import 'data/datasources/mongodb/events_mongodb.dart';
 import 'data/datasources/mongodb/promotions_mongodb.dart';
 
 void main() async{
-  var promotionsdb = PromotionsMongodb();
+  var eventsdb = EventsMongodb();
   WidgetsFlutterBinding.ensureInitialized();
   await initialize();
-  await promotionsdb.getPromociones(); //DELETE THIS
+  await eventsdb.getARandomEvent(); //DELETE THIS
   runApp(const Festa());
 }
