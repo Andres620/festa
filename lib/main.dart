915b12495e7d1ed5515c30355a1bdb59a4b9fa40
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-19 01:18:45 -0500

    [Add] Unit Test promotions

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,5 @@ import 'package:flutter/material.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initialize();
-<<<<<<< HEAD
-  await promotionsdb.getPromociones(); //DELETE THIS
-=======
->>>>>>> a9a602624b6f074617fb7861d187a9d095426589
   runApp(const Festa());
 }
--- test/promotions_test.dart
+++ test/promotions_test.dart
@@ -1,9 +1,36 @@
 import 'package:festa/data/datasources/mongodb/promotions_mongodb.dart';
+import 'package:festa/domain/models/promocion.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 void main(){
-  
-  test('get all promotions - promotions_mongodb', (){
+  test('get all promotions - promotions_mongodb', () async {
     var promotionsdb = PromotionsMongodb();
-    promotionsdb.getPromociones();
+    var response = await promotionsdb.getPromociones() ;
+    expect(response.length, 2);
+  });
+
+  test('json to promotion', () {
+    var promotionJson = {
+      "_id":ObjectId(),
+      "FechaInicio": DateTime.now(),
+      "FechaFin": DateTime.now(),
+      "Descripcion":"promotion test",
+      "Imagen":"imageTest.jpg"};
+
+    var response = Promocion.fromJson(promotionJson);
+    expect(response, isA<Promocion>());
+  });
+
+    test('promotion to json', () {
+    var promotion = Promocion(
+      id: ObjectId(),
+      fechaInicio: DateTime.now(),
+      fechaFin: DateTime.now(),
+      descripcion: "test promotion",
+      imagen: "imageTest.jpg"
+    );
+
+    var response = promosionToJson(promotion);
+    expect(response, isA<String>());
   });
 }
