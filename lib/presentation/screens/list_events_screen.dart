3ee53710829766f0db119a2e3723a9fdd7404026
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-11-20 21:08:36 -0500

    [Add] BottomNavigationBar

--- lib/presentation/screens/list_events_screen.dart
+++ lib/presentation/screens/list_events_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_final_fields, unused_field
+
 import 'package:festa/presentation/screens/second_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,11 +25,15 @@ class _ListEventsScreenState extends State<ListEventsScreen> {
     super.initState();
   }
 
-
+  int _actualPage = 0;
+  List<Widget> _pages = [
+    HomePage(),
+    ListPromocionesScreen()
+  ];
   ///Is the main method of the view and is responsible for displaying the information of each event.
   @override
   Widget build(BuildContext context) {
-    final providerEventos = Provider.of<EventProvider>(context);
+    
     return Scaffold(
         appBar: AppBar(
             title: const Text('Eventos'),
@@ -36,31 +42,72 @@ class _ListEventsScreenState extends State<ListEventsScreen> {
         //   child: Column(
         //       children: [const Text("Opcion 1"), const Text("Opcion 2")]),
         // ),
-        body: FutureBuilder<List<Evento>>(
-          future: providerEventos.cuListEvents.getAllEvents(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: ((context, index) {
-                    Evento event = snapshot.data![index];
-                    return _buidCards(event);
-                  }));
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.hasError}');
-            }
-            return const Center(
-              child: SizedBox(
-                width: 20,
-                height: 20,
-                child: CircularProgressIndicator(),
-              ),
-            );
+        body: _pages[_actualPage],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index){
+            setState(() {
+              _actualPage = index;
+            });
           },
-        ));
+          currentIndex: _actualPage,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(icon: Icon(Icons.attach_money), label: "promotions"),
+            BottomNavigationBarItem(icon: Icon(Icons.rocket_launch), label: "event_club")
+          ],
+        ),);
   }
 
-  /// Builds the structure to display the information of an event.
+
+  void listenToNotification() =>
+      onNotificationClick.stream.listen(onNoticationListener);
+
+  /// It loads the notification payload and sends it to a second view.
+  void onNoticationListener(String? payload) {
+    if (payload != null && payload.isNotEmpty) {
+      print('payload $payload');
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: ((context) => SecondScreen(payload: payload))));
+    }
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    final providerEventos = Provider.of<EventProvider>(context);
+    return Scaffold(
+      body:   FutureBuilder<List<Evento>>(
+                future: providerEventos.cuListEvents.getAllEvents(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: ((context, index) {
+                          Evento event = snapshot.data![index];
+                          return _buidCards(event);
+                        }));
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.hasError}');
+                  }
+                  return const Center(
+                    child: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                },
+              ),
+    );
+  }
+  
+/// Builds the structure to display the information of an event.
   Widget _buidCards(Evento evento) {
     return Container(
         clipBehavior: Clip.none,
@@ -94,24 +141,5 @@ class _ListEventsScreenState extends State<ListEventsScreen> {
                 )
               ],
             )));
-  }
-
-  void listenToNotification() =>
-      onNotificationClick.stream.listen(onNoticationListener);
-
-<<<<<<< HEAD
-=======
-
->>>>>>> d6d1eb96048fdf7387a8d461787fa762a1c6ffeb
-  /// It loads the notification payload and sends it to a second view.
-  void onNoticationListener(String? payload) {
-    if (payload != null && payload.isNotEmpty) {
-      print('payload $payload');
-
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: ((context) => SecondScreen(payload: payload))));
-    }
   }
 }
