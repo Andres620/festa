e93cf8e94b3f08d97583a0db99cf5f1bf67ca972
Juan Ospina <juan.1701720807@ucaldas.edu.co>
2022-11-20 20:04:30 -0500

    Comentarios eventos por discoteca

--- lib/data/datasources/test_local/Event/eventos_por_discoteca.dart
+++ lib/data/datasources/test_local/Event/eventos_por_discoteca.dart
@@ -3,15 +3,17 @@ import 'package:festa/data/datasources/test_local/Event/events_generator.dart';
 import '../../../../domain/models/evento.dart';
 import '../../../../domain/repositories/eventos_por_discoteca_repository.dart';
 
+/// Class that implements the events contract interacting with the fake db
+/// This class filters the events created by the id of a disco.
 class EventosPorDiscotecadbFake extends EventosPorDiscotecaRepository {
   EventosPorDiscotecadbFake({required super.idDiscoteca});
 
   @override
   Future<List<Evento>> getEventosPorDiscoteca() async {
-    //_AnimatedMovies = AllMovies.where((i) => i.isAnimated).toList();
-
     List<Evento> response = EventsGenerator(numberEvents: 3).createEvents();
 
+    //An event can be assigned to a few clubs,so it is scrolledthrough
+    //and the events is selected if the club id corresponds to the entered id.
     List<Evento> result = [
       for (var evento in response)
         for (var disco in evento.discoteca!)
