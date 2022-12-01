a182657026712fb89fe17743091af05065587e12
Andres620 <andres.1701625573@ucaldas.edu.co>
2022-12-01 00:28:09 -0500

    [Add] getEventsByDisco

--- lib/domain/repositories/events_repository.dart
+++ lib/domain/repositories/events_repository.dart
@@ -1,3 +1,5 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
 import '../models/evento.dart';
 
 /// Contract that must be implemented for an event repository to 
@@ -8,4 +10,5 @@ abstract class EventsRepository{
   ///   to obtain them so that they can be manipulated as event type objects.
   Future<List<Evento>> getEvents();
   Future<Evento> getARandomEvent();
+  Future<List<Evento>> getEventsByDisco(ObjectId discoId);
 }
